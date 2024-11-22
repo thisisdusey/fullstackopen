4fc7033fbf7df9d1d5c04c4d82cbbd4426fd0698
thisisdusey <116038724+thisisdusey@users.noreply.github.com>
2024-11-22 13:11:35 -0600

    1.6

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,64 +1,43 @@
+import { useState } from 'react'
+
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick =()=>{
+    setGood(good+1)
   }
-  
+
+  const handleNeutralClick =()=>{
+    setNeutral(neutral+1)
+  }
+
+  const handleBadClick =()=>{
+    setBad(bad+1)
+  }
 
 
-const Header = (props) =>{
-  return(
+  return (
     <div>
-      <h1>{props.course} </h1>
+      <h1>give Feedback</h1>
+      <Button onClick={handleGoodClick}buttonName='good'/>
+      <Button onClick={handleNeutralClick}buttonName='neutral'/>
+      <Button onClick={handleBadClick}buttonName = 'bad'/>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
-const Content = (props) =>{
+
+const Button = (props) =>{
   return(
-    <div>
-<Part part = {course.parts[0].name} exercises={course.parts[0].exercises}/> 
-<Part part = {course.parts[1].name} exercises={course.parts[1].exercises}/> 
-<Part part = {course.parts[2].name} exercises={course.parts[2].exercises}/> 
-
-
-
-</div>
+    <button onClick={props.onClick}> {props.buttonName}</button>
   )
-}
-const Part = (props) =>{
-  return(
-    <div>
-      <p>This part is {props.part} with {props.exercises} excercises</p>
-    </div>
-  )
-}
-const Total = (props) =>{
-return(
-  <div>
-    <p>Number of exercises: {props.parts}</p>
-  </div>
-)
-}
-return (
-  <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises} />
-  </div>
-)
 }
 
 export default App
