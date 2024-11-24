053a9a4621f24534133c749b6affaaeb24362c70
thisisdusey <116038724+thisisdusey@users.noreply.github.com>
2024-11-24 11:58:38 -0600

    fileRestructure

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,59 +1,64 @@
-import { useState } from 'react'
-
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-  const [vote, setVoteCount] = useState(0)
-  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
-
-const handleOnClick=()=>{
-  const randomIndex = Math.floor(Math.random() * anecdotes.length)
-  setSelected(randomIndex)
-}
-
-const handleVoteClick=()=>{
-  const votesCopy = [...votes];
-  votesCopy[selected]+=1
-  setVotes(votesCopy)
-}
-
-const mostVotesIndex = votes.indexOf(Math.max(...votes))
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
 
 
-  return (
-    <div>
-    <h1>Anecdote of the day</h1>
-    <div>{anecdotes[selected]}</div>
-    <div>has {votes[selected]} votes</div>
-    <Button onClick={handleVoteClick} text= 'vote'/>
-    <Button onClick={handleOnClick} text= 'next anecdote'/> 
-    
-    <h1>Anecdote with the most votes</h1>
-    {Math.max(...votes)>0 ? (
-      <div>
-      <p>{anecdotes[mostVotesIndex]}</p>
-      <p>has {votes[mostVotesIndex]} votes</p>
-    </div>
-    ) : (
-    <p>No votes yet.</p>
-    )}
-    </div>
-    )}
-
-const Button = (props) =>{
+const Header = (props) =>{
   return(
-<button onClick={props.onClick}>{props.text}</button>
+    <div>
+      <h1>{props.course} </h1>
+    </div>
   )
+}
+const Content = (props) =>{
+  return(
+    <div>
+<Part part = {course.parts[0].name} exercises={course.parts[0].exercises}/> 
+<Part part = {course.parts[1].name} exercises={course.parts[1].exercises}/> 
+<Part part = {course.parts[2].name} exercises={course.parts[2].exercises}/> 
+
+
+
+</div>
+  )
+}
+const Part = (props) =>{
+  return(
+    <div>
+      <p>This part is {props.part} with {props.exercises} excercises</p>
+    </div>
+  )
+}
+const Total = (props) =>{
+return(
+  <div>
+    <p>Number of exercises: {props.parts}</p>
+  </div>
+)
+}
+return (
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises} />
+  </div>
+)
 }
 
 export default App
---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+
+const StatisticLine = (props) => {
+  return(
+    <div>{props.text} :{props.value}</div>
+  )
+}
+
+const Statistics = ({ good, neutral, bad, total, average, positive }) =>{
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  }
+
+  return (
+    <div>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="All" value={total} />
+      <StatisticLine text="Average" value={average.toFixed(2)} />
+      <StatisticLine text="Positive" value={`${positive.toFixed(2)} %`} />
+    </div>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick =()=>{
+    setGood(good+1)
+  }
+
+  const handleNeutralClick =()=>{
+    setNeutral(neutral+1)
+  }
+
+  const handleBadClick =()=>{
+    setBad(bad+1)
+  }
+
+  const total = (good+neutral+bad)
+
+  const average = (good-bad)/total
+  const positive = Number.isFinite(good / total)*100 ? good / total : 0
+
+  return (
+    <div>
+      <h1>give Feedback</h1>
+      <Button onClick={handleGoodClick}buttonName='good'/>
+      <Button onClick={handleNeutralClick}buttonName='neutral'/>
+      <Button onClick={handleBadClick}buttonName = 'bad'/>
+      <h1>statistics</h1>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positive={positive}
+      />
+    </div>
+
+)}
+
+const Button = (props) =>{
+  return(
+    <button onClick={props.onClick}> {props.buttonName}</button>
+  )
+}
+
+
+
+export default App
