240c70eb926882cc64e9b84e4f1b6f8130fb9b3b
thisisdusey <116038724+thisisdusey@users.noreply.github.com>
2024-11-25 11:28:07 -0600

    2.1

---
+++ part2/courseinfo2/src/App.jsx
@@ -0,0 +1,69 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
+
+
+const Header = (props) =>{
+  return(
+    <div>
+      <h1>{props.course} </h1>
+    </div>
+  )
+}
+const Content = (props) =>{
+  return(
+    <div>
+<Part part = {course.parts[0].name} exercises={course.parts[0].exercises}/> 
+<Part part = {course.parts[1].name} exercises={course.parts[1].exercises}/> 
+<Part part = {course.parts[2].name} exercises={course.parts[2].exercises}/> 
+
+
+
+</div>
+  )
+}
+const Part = (props) =>{
+  return(
+    <div>
+      <p>This part is {props.part} with {props.exercises} excercises</p>
+    </div>
+  )
+}
+const Total = (props) =>{
+return(
+  <div>
+    <p>Number of exercises: {props.parts}</p>
+  </div>
+)
+}
+
+const Course = ({course}) =>{
+  return(
+    <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises} />
+    </div>
+  )
+
+}
+return <Course course={course} />
+}
+
+export default App
---
+++ playground/src/App.jsx
@@ -0,0 +1,19 @@
+import Note from './components/Note'
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <li key={note.id}>
+            {note.content}
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+
+
+export default App
