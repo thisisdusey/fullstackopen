c298f95c0b45455bc634214eb06e1281c8372066
thisisdusey <116038724+thisisdusey@users.noreply.github.com>
2024-11-25 11:43:28 -0600

    2.3

--- part2/courseinfo2/src/App.jsx
+++ part2/courseinfo2/src/App.jsx
@@ -13,6 +13,10 @@ const App = () => {
       {
         name: 'State of a component',
         exercises: 14
+      },
+      {
+        name: 'Redux',
+        exercises: 11
       }
     ]
   }
@@ -32,6 +36,7 @@ const Content = (props) =>{
 <Part part = {course.parts[0].name} exercises={course.parts[0].exercises}/> 
 <Part part = {course.parts[1].name} exercises={course.parts[1].exercises}/> 
 <Part part = {course.parts[2].name} exercises={course.parts[2].exercises}/> 
+<Part part = {course.parts[3].name} exercises={course.parts[3].exercises}/> 
 
 
 
@@ -41,14 +46,14 @@ const Content = (props) =>{
 const Part = (props) =>{
   return(
     <div>
-      <p>This part is {props.part} with {props.exercises} excercises</p>
+      <p>{props.part} with {props.exercises} excercises</p>
     </div>
   )
 }
 const Total = (props) =>{
 return(
   <div>
-    <p>Number of exercises: {props.parts}</p>
+    <p><b>Number of exercises: {props.parts}</b></p>
   </div>
 )
 }
@@ -58,11 +63,13 @@ const Course = ({course}) =>{
     <div>
     <Header course={course.name} />
     <Content parts={course.parts} />
-    <Total parts={course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises} />
+    <Total parts={total} />
     </div>
   )
-
 }
+
+const total = course.parts.reduce((accumulator, part) => {return accumulator +part.exercises},0);
+
 return <Course course={course} />
 }
 
