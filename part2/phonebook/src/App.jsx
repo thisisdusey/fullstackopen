1dc803c7baf64744dc4aaa394a1bcd777690099d
thisisdusey <116038724+thisisdusey@users.noreply.github.com>
2024-12-02 13:59:21 -0600

    upto2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,66 +1,113 @@
-import { useState,useEffect } from 'react'
-import axios from 'axios'
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
-import Filter from './components/Filter'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import PersonForm from "./components/PersonForm";
+import Person from "./components/Person";
+import Filter from "./components/Filter";
+import phonebookService from "./services/phonebookService";
 
 const App = (props) => {
-  const [persons, setPersons] = useState([]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [search, setSearch] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
-  useEffect(() =>{
-    axios
-    .get('http://localhost:3001/persons')
-    .then(response=> {
-      setPersons(response.data)
-    })
-  },[])
+  useEffect(() => {
+    phonebookService.getAll().then((initialPeople) => {
+      setPersons(initialPeople);
+    });
+  }, []);
 
-  const addPerson = (event) =>{
-    event.preventDefault()
-    const nameExists = persons.some(person=>person.name===newName)
-    const numberExists = persons.some(person=>person.number===newNumber)
-    if(numberExists && nameExists){
-      alert(`${newName} with ${newNumber} is already added to the phonebook`)
-      return
-    }
+  const addPerson = (event) => {
+    event.preventDefault();
+    const existingPerson = persons.find((person) => person.name === newName);
+    // const person = persons.find(p=>p.id===id)
+    // const changedPerson = { ...person, number: newNumber}
+    // const numberExists = persons.some(person=>person.number===newNumber)
+
     const personObject = {
-      name : newName,
-      number: newNumber
+      name: newName,
+      number: newNumber,
+    };
+
+    if (existingPerson) {
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace the old number with a new one?`
+        )
+      ) {
+        phonebookService
+          .update(existingPerson.id, personObject)
+          .then((returnedPerson) => {
+            setPersons(
+              persons.map((person) =>
+                person.id === existingPerson.id ? returnedPerson : person
+              )
+            );
+            setNewName("");
+            setNewNumber("");
+          })
+
+          .catch((error) => {
+            alert(
+              `The person '${existingPerson.name}' was already deleted from the server.`
+            );
+            setPersons(persons.filter((p) => p.id !== existingPerson.id));
+          });
+      }
+      return;
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
-  }
+
+    phonebookService
+      .create(personObject) // POST request to create a new person
+      .then((response) => {
+        setPersons(persons.concat(response.data)); // Add the new person to state
+        setNewName("");
+        setNewNumber("");
+      })
+      .catch((error) => {
+        alert("Error adding person to the phonebook.");
+      });
+  };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-  
+    setNewName(event.target.value);
+  };
+
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
 
   const handleSearchChange = (event) => {
-    setSearch(event.target.value)
-  }
+    setSearch(event.target.value);
+  };
 
+  const handleDelete = (id) => {
+    const person = persons.find((p) => p.id === id);
 
+    if (window.confirm(`Delete ${person.name}?`)) {
+      phonebookService
+        .deletePerson(id)
+        .then(() => {
+          setPersons(persons.filter((p) => p.id !== id));
+        })
+        .catch((error) => {
+          alert(
+            `The person '${person.name}' was already deleted from the server.`
+          );
+          setPersons(persons.filter((p) => p.id !== id));
+        });
+    }
+  };
 
-  const filteredPersons = persons.filter(person => 
+  const filteredPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(search.toLowerCase())
-  )
+  );
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Filter 
-      value ={search} 
-      handleSearchChange = {handleSearchChange}
-      />
-      <h3>Add a new</h3>
+      <h1>Phonebook</h1>
+      <Filter value={search} handleSearchChange={handleSearchChange} />
+      <h2>Add a new</h2>
       <PersonForm
         addPerson={addPerson}
         newName={newName}
@@ -68,12 +115,10 @@ const App = (props) => {
         newNumber={newNumber}
         handleNumberChange={handleNumberChange}
       />
-      <h3>Numbers</h3>
-      <Persons
-      filteredPersons={filteredPersons}
-      />
+      <h2>Numbers</h2>
+      <Person handleDelete={handleDelete} filteredPersons={filteredPersons} />
     </div>
-  ) 
-}
+  );
+};
 
-export default App
+export default App;
---
+++ part2/phonebook/src/components/Person.jsx
@@ -0,0 +1,12 @@
+const Person = ({filteredPersons, handleDelete}) =>{
+return(
+    <ul>
+    {filteredPersons.map(person => 
+      <li key={person.name}>{person.name} {person.number}
+    <button onClick={() => handleDelete(person.id)}>delete</button>
+    </li>
+    )}
+  </ul>  
+)
+}
+export default Person
